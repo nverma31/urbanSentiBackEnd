d1ff1240b054ddd7ccd35351725811660e729fe6
JeffryLee <574395594@qq.com>
2016-09-15 01:53:37 -0400

    fix word2vec

--- keywordsSelection/myWord2vec.py
+++ keywordsSelection/myWord2vec.py
@@ -1,17 +1,20 @@
 import gensim, logging
+import sys
 import os
 from sklearn.cluster import KMeans
 import numpy as np
 
+# import gensim.models.word2vec
+
 class MySentences(object):
-    def __init__(self, dirname):
-        self.dirname = dirname
+    def __init__(self, fname):
+        self.fname = fname
 
     def __iter__(self):
-        for fname in os.listdir(self.dirname):
-            for line in open(os.path.join(self.dirname, fname)):
-                yield line.split()
+        for line in open(self.fname):
+            yield line.split()
 
-sentences = MySentences('./term/') # a memory-friendly iterator
-model = gensim.models.word2vec(sentences , min_count=10, workers=4, size=200)
+sentences = MySentences('./termWord.txt') # a memory-friendly iterator
+print "started"
+model = gensim.models.Word2Vec(sentences, min_count=10, workers=4, size=200)
 model.save('modelOut.txt')
